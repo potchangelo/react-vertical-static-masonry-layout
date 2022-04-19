ea43f6cca1a71925e8429236e543334bf57481a8
Potchara Puttawanchai <potchangelo@gmail.com>
2022-04-19 16:18:20 +0700

    Image shuffle

--- src/pages/_ImageShuffle.js
+++ src/pages/_ImageShuffle.js
@@ -1,10 +1,51 @@
-import { Main } from '../layouts';
+import { useState } from 'react';
+import { ItemImage } from '../components';
+import { samplePhotos } from '../helpers';
+import { Main, Masonry, MasonryItem } from '../layouts';
+
+const breakpoints = [
+  { columns: 2, minWidth: 0, gap: 12, outerGap: 16 },
+  { columns: 3, minWidth: 500, gap: 16, outerGap: 20 },
+  { columns: 4, minWidth: 750, gap: 20, outerGap: [32, 20] },
+  { columns: 5, minWidth: 1000, gap: 20, outerGap: [32, 20] },
+];
 
 function _ImageShuffle() {
+  const [photos, setPhotos] = useState(getShufflePhotos());
+
+  function getShufflePhotos() {
+    const arr = [...samplePhotos];
+    for (let i = arr.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+  }
+
+  function shufflePhotos() {
+    setPhotos(getShufflePhotos());
+  }
+
+  const masonryItems = photos.map(photo => (
+    <MasonryItem key={photo.id} height={photo.height}>
+      <ItemImage photo={photo} />
+    </MasonryItem>
+  ));
   return (
-    <Main>
-      image shuffle
-    </Main>
+    <>
+      <Main>
+        <Masonry breakpoints={breakpoints}>
+          {masonryItems}
+        </Masonry>
+      </Main>
+      <button
+        className="button is-primary"
+        style={{ position: 'fixed', bottom: '12px', right: '12px', zIndex: 10000 }}
+        onClick={shufflePhotos}
+      >
+        <b>Shuffle</b>
+      </button>
+    </>
   );
 }
 
